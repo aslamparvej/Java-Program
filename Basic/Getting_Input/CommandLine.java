b18c3c3c8a0ed27f09fcc20783dca09dec914c47
Aslam Parvej <aslamparvej2@gmail.com>
2022-01-05 21:15:56 +0530

    Getting Input

---
+++ Basic/Getting_Input/CommandLine.java
@@ -0,0 +1,7 @@
+package Basic.Getting_Input;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        System.out.print("Hii " + args);
+    }
+}
---
+++ Basic/Getting_Input/ScannerClass.java
@@ -0,0 +1,24 @@
+package Basic.Getting_Input;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What's your name:");
+        String name = scan.nextLine();
+
+        System.out.println("What's your age:");
+        int age = scan.nextInt();
+        
+        System.out.println("Sex:");
+        String sex = scan.next();
+
+        System.out.println("Are you Indian");
+        boolean q = scan.nextBoolean();
+
+        System.out.println("Your name is "+name+" your are "+age+" years old"+" sex is " +sex);
+        System.out.println("are you Indian: "+q);
+    }
+}
